b3f74ffefd7a5423e4808e2d970d8cae263b6684
vazkido <vazkid02@gmail.com>
2025-03-05 17:22:13 -0500

    listen 3000

--- task2/server.js
+++ task2/server.js
@@ -5,5 +5,5 @@ const http = require('http');
 http.createServer (function(req, res){
     res.writeHead(200, {"Content-Type": "text/plain"});
     
-    const newPass 
-})
+    const newPass = res.end("Hello World/n");
+}).listen(3000)
